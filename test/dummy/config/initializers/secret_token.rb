7f434081ab30846c579d4ea98183362e503403c8
Dustin DeYoung <dustin@westlakedesign.com>
2012-06-15 09:22:16 -0400

    Move test dummy to spec dummy

--- test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '9af0115c35e3abcfd4dba71a6bdb8d456f312311cc3767b28dc8cfde3a8988833b7079aba320adba459332218acb71c7aee981ef976ec007cee19fabd7f31bdb'
--- test/dummy/config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-
-  mount SpudPhotos::Engine => "/spud_photos"
-end
