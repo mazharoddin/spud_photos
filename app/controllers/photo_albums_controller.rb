622174b093a2d5ce18010c98fe9405707bcd6fd7
Greg Woods <greg@westlakedesign.com>
2012-12-02 23:07:47 -0500

    when navigating to a gallery show page, if that gallery only has one album, simply redirect right to that album

--- app/controllers/photo_albums_controller.rb
+++ app/controllers/photo_albums_controller.rb
@@ -3,6 +3,10 @@ class PhotoAlbumsController < ApplicationController
   respond_to :html, :json, :xml
   layout Spud::Photos.base_layout
 
+  if Spud::Photos.galleries_enabled
+    before_filter :get_gallery
+  end
+
   after_filter :only => [:show, :index] do |c|
     if Spud::Photos.enable_full_page_caching
       c.cache_page(nil, nil, false)
@@ -10,24 +14,22 @@ class PhotoAlbumsController < ApplicationController
   end
 
   def index
-    if params[:photo_gallery_id]
-      @photo_gallery = SpudPhotoGallery.find_by_url_name(params[:photo_gallery_id])
-      if @photo_gallery.blank?
-        flash[:error] = 'Photo gallery could not be found!'
-        redirect_to photo_galleries_path and return false
-      else
-        @photo_albums = @photo_gallery.albums.order('created_at desc')
-      end
+    if @photo_gallery
+      @photo_albums = @photo_gallery.albums.order('created_at desc')
     else
       @photo_albums = SpudPhotoAlbum.order('created_at desc')
     end
-    respond_with @photo_albums
+    if request.format.html? && @photo_albums.length == 1
+      redirect_to photo_gallery_photo_album_path(@photo_gallery.url_name, @photo_gallery.albums.first.url_name)
+    else
+      respond_with @photo_albums
+    end
   end
 
   def show
     @photo_album = SpudPhotoAlbum.find_by_url_name(params[:id])
     if @photo_album.blank?
-      flash[:error] = "Post not found!"
+      flash[:error] = "Could not find the requested Photo Album"
       if params[:photo_gallery_id]
         redirect_to photo_gallery_photo_albums_path(params[:photo_gallery_id])
       else
@@ -35,6 +37,21 @@ class PhotoAlbumsController < ApplicationController
       end
     else
       respond_with @photo_album
+    end
+  end
+
+private
+
+  def get_gallery
+    @photo_gallery = SpudPhotoGallery.find_by_url_name(params[:photo_gallery_id])
+    if @photo_gallery.blank?
+      flash[:error] = "Could not find the requested Photo Gallery"
+      if request.xhr?
+        render :nothing => true, :status => 404
+      else
+        redirect_to photo_galleries_path
+      end
+      return false
     end
   end
 
